dbffc2c319a76d13c2d8e332f8582859be7b7c0a
Светлана <156022042+elleune@users.noreply.github.com>
2025-05-02 13:10:23 +0300

    Update ShareItApp.java

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItApp {
-        @PostConstruct
-    void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC")); 
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ShareItApp.class, args);
     }
